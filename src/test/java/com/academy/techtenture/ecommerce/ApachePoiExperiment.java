d99965f80176efe56066b61f03725588d4c777dc
TairovichJR <yokubov.omonjon@gmail.com>
2022-06-04 13:03:00 -0400

    apache poi and testng dataprovider

---
+++ src/test/java/com/academy/techtenture/ecommerce/ApachePoiExperiment.java
@@ -0,0 +1,29 @@
+package com.academy.techtenture.ecommerce;
+
+import com.academy.techcenture.ecommerce.utils.ExcelReader;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
+public class ApachePoiExperiment {
+
+
+
+    @Test(dataProvider = "newUsersData")
+    public void experimentApachePoiData( Map<String,String> users  ){
+
+        System.out.println(users);
+
+    }
+
+
+    @DataProvider(name = "newUsersData")
+    public Object[][] getNewUsersData(){
+        //                                                  path the the ecommerce excel document               sheet name
+        ExcelReader reader = new ExcelReader("src/main/resources/testData/ecommerce.xlsx", "newUsers");
+        return reader.getData();
+    }
+
+
+}
